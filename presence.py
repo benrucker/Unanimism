7a3f73ceb02a1133c2982c036865d1ab8f82d80a
Ben Rucker <brucker117@gmail.com>
2020-06-07 17:25:44 -0400

    add activity setting

---
+++ presence.py
@@ -0,0 +1,37 @@
+import asyncio
+from itertools import cycle
+import discord
+from discord.enums import ActivityType
+from discord.ext import commands, tasks
+
+PRESENCES = [(ActivityType.watching,  'for u.help'),
+             (ActivityType.playing,   'Democracy'),
+             (ActivityType.listening, 'your complaints'),
+            ]
+
+def setup(bot):
+    bot.add_cog(Presence(bot))
+
+
+class Presence(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.presences = cycle(PRESENCES)
+        self.change_presence_task.start()
+
+    def cog_unload(self):
+        self.change_presence_task.cancel()
+
+    @tasks.loop(seconds=20)
+    async def change_presence_task(self):
+        if not self.bot.is_closed():
+            curr = next(self.presences)
+            await self.bot.change_presence(activity=discord.Activity(
+                type=curr[0], name=curr[1]
+            ))
+            await asyncio.sleep(30)
+
+    @change_presence_task.before_loop
+    async def before_presence_task(self):
+        await self.bot.wait_until_ready()
