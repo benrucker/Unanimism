287e2a3c26ec1e259e1b6c4ac97d75377686b85f
Ben Rucker <brucker117@gmail.com>
2020-06-22 17:33:27 -0400

    refactor match match new task naming scheme

--- presence.py
+++ presence.py
@@ -19,19 +19,19 @@ class Presence(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.presences = cycle(PRESENCES)
-        self.change_presence_task.start()
+        self.task_change_presence.start()
 
     def cog_unload(self):
-        self.change_presence_task.cancel()
+        self.task_change_presence.cancel()
 
     @tasks.loop(seconds=20)
-    async def change_presence_task(self):
+    async def task_change_presence(self):
         if not self.bot.is_closed():
             curr = next(self.presences)
             await self.bot.change_presence(activity=discord.Activity(
                 type=curr[0], name=curr[1]
             ))
 
-    @change_presence_task.before_loop
-    async def before_presence_task(self):
+    @task_change_presence.before_loop
+    async def task_before_change_presence(self):
         await self.bot.wait_until_ready()
